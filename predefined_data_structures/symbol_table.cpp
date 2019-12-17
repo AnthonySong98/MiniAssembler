af653a10a381609fa0ceb6e5ab8ad8306c98cd1b
CoChaCoRa <1790743147@qq.com>
2019-12-17 19:48:15 +0800

    Implement symbol_table.cpp
    实现了symbol_table的条目添加、查询键和读取值

--- predefined_data_structures/symbol_table.cpp
+++ predefined_data_structures/symbol_table.cpp
@@ -8,17 +8,18 @@ SYMBOL_TABLE::SYMBOL_TABLE(const unordered_map<string, unsigned int> &symbol_tab
 
 SYMBOL_TABLE::SYMBOL_TABLE() {}
 
-// TODO: implement addEntryToSymbolTable
+// Implemented by Chen on 2019/12/17
+
 void SYMBOL_TABLE::addEntryToSymbolTable(string _symbol, unsigned int _address){
-
+    this->symbol_table.insert(make_pair(_symbol,_address));
 }
 
-// TODO: implement containsSymbol
 bool SYMBOL_TABLE::containsSymbol(string _symbol){
-    return false;
+    if(this->symbol_table.find(_symbol)!=this->symbol_table.end()) return true;
+    else return false;
 }
 
-//TODO: implement getAddress
 unsigned int SYMBOL_TABLE::getAddress(string _symbol){
-    return 0;
+    if(this->symbol_table.find(_symbol)!=this->symbol_table.end()) return symbol_table[_symbol];
+    else return NULL;
 }
